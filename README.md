45fdb8a50e28845a543253d1a310bfee0ad2a955
Fabrício Vale <fabriciovale18@gmail.com>
2021-08-07 19:25:30 -0300

    Game

---
+++ README.md
@@ -0,0 +1,11 @@
+# ADIVINHE O NÚMERO
+
+*O usuário precisa acertar um número gerado aleatoriamente.*
+
+**Tecnologias utilizadas:**
+- Python 3.9;
+- Flask;
+- REST;
+- Random;
+
+![alt text](jogo.png) ![alt text](static/acertou.png) ![alt text](static/errou.png)
---
+++ adivinhe.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from random import randint
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        return render_template('home.html')  # Página inicial
+    else:
+        numero_computador = randint(1, 10)  # Range de números, é possível altera-los
+        valor = int(request.form.get('valor'))  # Recebe o valor informado pelo usuário
+
+        # Faz a condição se o usuário escolheu o mesmo número do computador
+        if numero_computador == valor:
+            return render_template('acertou.html')  # Se acertou, retorna essa página
+        else:
+            return render_template('errou.html')  # Se errou, retorna essa página
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
