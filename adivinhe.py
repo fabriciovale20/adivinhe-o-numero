b65fe3ce558fd050922b2e1a17a730a85f00e969
Fabrício Vale <fabriciovale18@gmail.com>
2021-08-08 00:09:42 -0300

    • Atualizado favicon; • Correção de direcionamento quando input enviado sem valor

--- adivinhe.py
+++ adivinhe.py
@@ -6,19 +6,23 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    primeiro_valor = 1
-    ultimo_valor = 10
-    if request.method == 'GET':
-        return render_template('home.html')  # Página inicial
-    else:
-        numero_computador = randint(primeiro_valor, ultimo_valor)  # Range de números, é possível altera-los
-        valor = int(request.form.get('valor'))  # Recebe o valor informado pelo usuário
+    while True:
+        try:
+            primeiro_valor = 1  # Valor inicial, alterável
+            ultimo_valor = 10  # Valor final, alterável
+            if request.method == 'GET':
+                return render_template('home.html')  # Página inicial
+            else:
+                numero_computador = randint(primeiro_valor, ultimo_valor)  # Range de números, é possível altera-los acima
+                valor = int(request.form.get('valor'))  # Recebe o valor informado pelo usuário
 
-        # Faz a condição se o usuário escolheu o mesmo número do computador
-        if numero_computador == valor:
-            return render_template('acertou.html')  # Se acertou, retorna essa página
-        else:
-            return render_template('errou.html')  # Se errou, retorna essa página
+                # Faz a condição se o usuário escolheu o mesmo número do computador
+                if numero_computador == valor:
+                    return render_template('acertou.html')  # Se acertou, retorna essa página
+                else:
+                    return render_template('errou.html')  # Se errou, retorna essa página
+        except ValueError:
+            return render_template('home.html')  # Página inicial
 
 
 if __name__ == '__main__':
